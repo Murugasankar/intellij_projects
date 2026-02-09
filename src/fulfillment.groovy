7fd64bbf328e02ae378dadd8526a5a518f1d9a62
Murugasankar <murugasankar@gmail.com>
2026-02-09 15:38:58 +0530

    changes

---
+++ src/fulfillment.groovy
@@ -0,0 +1,16 @@
+class fulfillment {
+
+// Sample Groovy Program
+    class HelloGroovy {
+        static void main(String[] args) {
+            println "Hello, Groovy!"
+
+            // Variables
+            def name = "Murugasankar"
+            def age = 25
+
+            println "Name: $name"
+            println "Age: $age"
+
+        }
+    }}
